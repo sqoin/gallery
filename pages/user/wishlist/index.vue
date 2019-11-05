e2738fa86ffc780c91aeafeffc951d1e9d332af2
walidbahrii <bahriwalid19@gmail.com>
2019-11-05 13:56:31 +0100

    update wishlist

--- pages/user/wishlist/index.vue
+++ pages/user/wishlist/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h3 class="title">{{ pageTitle }}</h3>
       <div class="row">
-      <div class="card col-lg-4 is-one-quarter" v-for="product in productsInWishlist" :key="product.id">
+      <div class="col-lg-4 col-md-4 col-6" v-for="product in productsInWishlist" :key="product.id">
         <VmProductsList :product="product"></VmProductsList>
       </div>
       <div class="section" v-if="productsInWishlist.length === 0">
