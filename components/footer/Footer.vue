ac6f6a1566bb2f02c2657b405c877fd687770a1c
walidbahrii <bahriwalid19@gmail.com>
2019-11-15 13:47:58 +0100

    update buy

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -41,7 +41,7 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 				
-					<p class="h6">&copy All right Reversed.<a class="ml-2" href="https://www.sunlimetech.com" target="_blank">SQOIN</a> 2019</p>
+					<p class="h6">&copy All right Reversed.<a class="ml-2" href="https://sqoin.us/coin/" target="_blank">SQOIN</a> 2019</p>
 				</div>
 				</hr>
 			</div>	
--- pages/product_detail/_id.vue
+++ pages/product_detail/_id.vue
@@ -1,34 +1,42 @@
 
 
 <template>
-
-  <div class ="container">
+  <div class="container">
     <article class="tile is-child notification has-text-warning">
-        <h2 class="title is-4">{{ product.title }}</h2>
+      <h2 class="title is-4">{{ product.title }}</h2>
 
-        <div class="columns is-vcentered">
-        
-        
-    
-           <div class="box16 ">
- 
-                        <img class="image" :src="product.image">
-                         <div class="box-content">
-                            <h3 class="title">{{product.owner}}</h3>
-                            
-                            <ul class="social">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            </ul>
-                        </div>
-            </div>
-         
-          
-       
-           <div class="column has-text-centered">
-          
+      <div class="columns is-vcentered">
+        <div class="box16">
+          <img class="image" :src="product.image" />
+          <div class="box-content">
+            <h3 class="title">{{product.owner}}</h3>
+
+            <ul class="social">
+              <li>
+                <a href="#">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-google-plus"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="column has-text-centered">
           <div class="card-content__price">
             <span class="title is-3">
               <strong>{{ product.price }}&dollar;</strong>
@@ -70,7 +78,6 @@
         </div>
       </div>
     </article>
-
   </div>
 </template>
 
@@ -84,7 +91,6 @@ export default {
 
   data() {
     return {
-         
       addToCartLabel: "Buy",
       removeFromCartLabel: "paid",
       addToFavouriteLabel: "Add to favourite",
@@ -97,7 +103,6 @@ export default {
   },
 
   mounted() {
-
     this.product = this.$store.getters.getProductById(this.$route.params.id);
     this.selected = this.product.quantity;
 
@@ -105,13 +110,11 @@ export default {
       this.quantityArray.push(i);
     }
 
-
-     if (window.location.href.indexOf("txid") > -1) {
-       
-       this.$store.commit('purchase', this.product.id);
-       //alert("paid "+ this.product.isPurchased)
-         //this.$store.commit(this.product.isPurchased, true);
-     }
+    if (window.location.href.indexOf("txid") > -1) {
+      this.$store.commit("purchase", this.product.id);
+      //alert("paid "+ this.product.isPurchased)
+      //this.$store.commit(this.product.isPurchased, true);
+    }
   },
 
   computed: {
@@ -121,15 +124,12 @@ export default {
   },
 
   methods: {
-
-    purchasedProduct(){
-    if (product.isPurchased || (window.location.href.indexOf("txid") > -1) ){
-      return true;
-    }
-    else {
-      return false;
-    }
-
+    purchasedProduct() {
+      if (product.isPurchased || window.location.href.indexOf("txid") > -1) {
+        return true;
+      } else {
+        return false;
+      }
     },
     addToCart(id) {
       let data = {
@@ -174,43 +174,134 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-  .card-content {
-    
-
-    &__text {
-      margin: 15px 0;
-    }
-   
-
+.card-content {
+  &__text {
+    margin: 15px 0;
   }
-.box16
-{text-align:center;
-color:#fff;
-position:relative}
-.box16 .box-content,.box16:after{width:50%;position:absolute;left:0}
-.box16:after{content:"";height:100%;background:linear-gradient(to bottom,rgba(0,0,0,0) 0,rgba(0,0,0,.08) 69%,rgba(0,0,0,.76) 100%);top:0;transition:all .5s ease 0s}
-.box16 .post,.box16 .title{transform:translateY(145px);transition:all .4s cubic-bezier(.13,.62,.81,.91) 0s}
-.box16:hover:after{background:linear-gradient(to bottom,rgba(0,0,0,.01) 0,rgba(0,0,0,.09) 11%,rgba(0,0,0,.12) 13%,rgba(0,0,0,.19) 20%,rgba(0,0,0,.29) 28%,rgba(0,0,0,.29) 29%,rgba(0,0,0,.42) 38%,rgba(0,0,0,.46) 43%,rgba(0,0,0,.53) 47%,rgba(0,0,0,.75) 69%,rgba(0,0,0,.87) 84%,rgba(0,0,0,.98) 99%,rgba(0,0,0,.94) 100%)}
-
-.box16 .box-content{padding:20px;margin-bottom:20px;bottom:0;z-index:1}
-.box16 .title{font-size:22px;font-weight:700;text-transform:uppercase;margin:0 0 10px}
-
-.box16 .social li a,.box17 .icon li a{border-radius:20%;font-size:20px;}
-.box16:hover .post,.box16:hover .title{transform:translateY(0)}
-.box16 .social{list-style:none;padding:0 0 5px;margin:40px 0 25px;opacity:0;position:relative;transform:perspective(500px) rotateX(-90deg) rotateY(0) rotateZ(0);transition:all .6s cubic-bezier(0,0,.58,1) 0s}
-.box16:hover .social{opacity:1;transform:perspective(500px) rotateX(0) rotateY(0) rotateZ(0)}
-.box16 .social:before{content:"";width:50px;height:2px;background:#fff;margin:0 auto;position:absolute;top:-23px;left:0;right:0}
-.box16 .social li{display:inline-block}
-.box16 .social li a{display:block;width:30px;height:30px;line-height:30px;background:#ffffff;margin-right:10px;transition:all .3s ease 0s}
-.box17 .icon li,.box17 .icon li a{display:inline-block}
-.box16 .social li a:hover{background:#bea041}
-.box16 .social li:last-child a{margin-right:0}
-.image{
-  width:460px;
-  height:500px;
 }
-@media only screen and (width:450px){.box16{margin-bottom:30px}
+.box16 {
+  text-align: center;
+  color: #fff;
+  position: relative;
+}
+.box16 .box-content,
+.box16:after {
+  width: 50%;
+  position: absolute;
+  left: 0;
+}
+.box16:after {
+  content: "";
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0,
+    rgba(0, 0, 0, 0.08) 69%,
+    rgba(0, 0, 0, 0.76) 100%
+  );
+  top: 0;
+  transition: all 0.5s ease 0s;
+}
+.box16 .post,
+.box16 .title {
+  transform: translateY(145px);
+  transition: all 0.4s cubic-bezier(0.13, 0.62, 0.81, 0.91) 0s;
+}
+.box16:hover:after {
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.01) 0,
+    rgba(0, 0, 0, 0.09) 11%,
+    rgba(0, 0, 0, 0.12) 13%,
+    rgba(0, 0, 0, 0.19) 20%,
+    rgba(0, 0, 0, 0.29) 28%,
+    rgba(0, 0, 0, 0.29) 29%,
+    rgba(0, 0, 0, 0.42) 38%,
+    rgba(0, 0, 0, 0.46) 43%,
+    rgba(0, 0, 0, 0.53) 47%,
+    rgba(0, 0, 0, 0.75) 69%,
+    rgba(0, 0, 0, 0.87) 84%,
+    rgba(0, 0, 0, 0.98) 99%,
+    rgba(0, 0, 0, 0.94) 100%
+  );
+}
+
+.box16 .box-content {
+  padding: 20px;
+  margin-bottom: 20px;
+  bottom: 0;
+  z-index: 1;
+}
+.box16 .title {
+  font-size: 22px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin: 0 0 10px;
+}
+
+.box16 .social li a,
+.box17 .icon li a {
+  border-radius: 20%;
+  font-size: 20px;
+}
+.box16:hover .post,
+.box16:hover .title {
+  transform: translateY(0);
+}
+.box16 .social {
+  list-style: none;
+  padding: 0 0 5px;
+  margin: 40px 0 25px;
+  opacity: 0;
+  position: relative;
+  transform: perspective(500px) rotateX(-90deg) rotateY(0) rotateZ(0);
+  transition: all 0.6s cubic-bezier(0, 0, 0.58, 1) 0s;
+}
+.box16:hover .social {
+  opacity: 1;
+  transform: perspective(500px) rotateX(0) rotateY(0) rotateZ(0);
+}
+.box16 .social:before {
+  content: "";
+  width: 50px;
+  height: 2px;
+  background: #fff;
+  margin: 0 auto;
+  position: absolute;
+  top: -23px;
+  left: 0;
+  right: 0;
+}
+.box16 .social li {
+  display: inline-block;
+}
+.box16 .social li a {
+  display: block;
+  width: 30px;
+  height: 30px;
+  line-height: 30px;
+  background: #ffffff;
+  margin-right: 10px;
+  transition: all 0.3s ease 0s;
+}
+.box17 .icon li,
+.box17 .icon li a {
+  display: inline-block;
+}
+.box16 .social li a:hover {
+  background: #bea041;
+}
+.box16 .social li:last-child a {
+  margin-right: 0;
+}
+.image {
+  width: 460px;
+  height: 500px;
+}
+@media only screen and (width: 450px) {
+  .box16 {
+    margin-bottom: 30px;
+  }
 }
 </style>
 
